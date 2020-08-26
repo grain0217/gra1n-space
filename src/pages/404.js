d7dad088c40d8ea52cdcbfe0df323c5a395e3d9c
guyu10 <guyu10@maoyan.com>
2020-08-26 16:37:01 +0800

    feat: add 404

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import Layout from '../components/Layout';
+
+export default () => {
+  return (
+    <Layout>
+      <h1>404</h1>
+      <h3>你穿越到了一个未知的领域!</h3>
+    </Layout>
+  );
+};
